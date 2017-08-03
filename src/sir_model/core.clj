f8ca2d24f3cb1ad3215bf5aaa8b76fb4340cf73f
hpcjkour <hpcjkour@hlrs.de>
2017-08-03 22:52:18 +0200

    Minor changes, cleaning up

--- src/sir_model/core.clj
+++ src/sir_model/core.clj
@@ -21,16 +21,8 @@
   ;create an instance of SIR-Compartments
   (def s (->SIR-Compartments 100 21 42))
 
-  ;create a compartments-map
-<<<<<<< HEAD
+  ;create and initialize compartments-map
   (def sc (create-and-init-compartments-map 4 90 20))
-=======
-  ;(def sc (initialize-compartments-map (create-compartments-map 4) 1000 100))
-  (def sc (create-and-init-compartments-map 4 90 20))
-  ;initially infected and susceptible
-  ;(def sc (assoc-in sc [:1 :I] 100))
-  ;(def sc (assoc-in sc [:1 :S] 1000))
->>>>>>> b65e1fe70c72d9dc4a2c34604e42d199cd4602f7
 
   (println sc)
 
