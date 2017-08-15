0d86adbd66bbd1be7b94c333090e31de707f2116
hpcjkour <hpcjkour@hlrs.de>
2017-08-15 12:31:42 +0200

    Fix cohort-test
    Wasn't working prooerly, now does. So far, the test fails.

--- test/sir_model/cohort_test.clj
+++ test/sir_model/cohort_test.clj
@@ -4,28 +4,36 @@
     [sir-model.cohort :refer :all]
     [sir-model.compartments :refer :all]))
 
-(with-test
-  (def init (create-and-init-compartments-map 7 1000 76))
-
-  (def compartments (progress 1 4 50 (start-cohort 1 init 2) 0.4))
-
-  (defn sum-compartments
-    ([comps t]
-     (+ (get-in comps [(keyword (str t)) :S]) (get-in comps [(keyword (str t)) :I]) (get-in comps [(keyword (str t)) :R]))) )
 
 
-  (defn test-sum
-    "Is sum of SIR-compartments for time t the same as for time t-1?"
-    [comps t]
-    (= (sum-compartments comps t) (sum-compartments comps (dec t))))
+
+(defn sum-compartments
+  ([comps t]
+   (+ (get-in comps [(keyword (str t)) :S]) (get-in comps [(keyword (str t)) :I]) (get-in comps [(keyword (str t)) :R]))))
 
 
-  (defn test-sums-recur
-    "Recursively compare sums of SIR-compartments over all timesteps"
-    ([comps] (test-sums-recur comps (count comps)))
-    ([comps t]
-     (if (= 2 t)
-       (test-sum comps 2)
-       (and (test-sum comps t) (test-sums-recur comps (dec t))))))
+(defn test-sums
+  "Recursively compare sums of SIR-compartments over all timesteps"
+  ([comps] (test-sums comps 1))
+  ([comps t]
+   (letfn
 
-  (is (test-sums-recur compartments)) "Consistency of numbers in SIR-compartments")
+     ;"Is sum of SIR-compartments for time t the same as for time t+1?"
+     [(test-sum
+        [comps t]
+        (= (sum-compartments comps t) (sum-compartments comps (inc t))))
+      ]
+
+     (if (= t (count comps))
+       true                                       ; is there a better way to end the recursion?
+       (and (test-sum comps t) (test-sums comps (inc t)))))))
+
+
+(deftest sums-in-progression
+  (let
+
+    [init (create-and-init-compartments-map 7 1000 76)
+     compartments (progress 1 4 50 (start-cohort 1 init 2) 0.4)]
+
+    (testing "check for equalitiy of compartment-sums over timesteps"
+      (is (test-sums compartments 1)))))
