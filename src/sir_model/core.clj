4c3602fe193bd9fb7c7d5fe09de71dab3a74c566
hpcjkour <hpcjkour@hlrs.de>
2017-12-07 10:57:44 +0100

    Change parameters for primary and secondary infections

--- src/sir_model/core.clj
+++ src/sir_model/core.clj
@@ -3,20 +3,31 @@
   (:require [clojure.java.io :as io]
             [clojure.data.csv :as csv]
             [util.functions :as util]
+            [sir-model.two-stage-poisson :as model]
             )
-  (:use [anglican [core :exclude [-main]] runtime emit stat]
-        sir-model.cohort))
+  (:use [anglican [core :exclude [-main]] runtime emit stat]))
 
-;(take-nth 5 (drop 500 (take 1000 samples))
+
+
+
+(def arg-map
+  {:t-max        40
+   :compartments [:S :I :R :primary :secondary]
+   :inits        {:S 10000 :I 200}
+   :prior-1      (uniform-continuous 0.6 0.8)
+   :prior-2      (uniform-continuous 1.5 2.5)
+   })
+
+(def samples (doquery :smc model/two-stage-poisson-query [arg-map model/form-and-prog] :number-of-particles 1000))
+(def burned (take-nth 4 (take 500 samples)))
 
 (defn -main
   "Probabilistic SIR-Model"
   [& args]
 
-  (def samples (doquery :smc simple-poisson-process-model [10 cohort-progression 70000000 30] :particles 1000))
-  (def burned (take-nth 10 (drop 100 (take 500 samples))))
-  (util.functions/write-seasons! burned :unused "data/testdat.csv")
+  (util.functions/write-seasons! burned util.functions/new-infections "data/testdat.csv") )
+(-main)
 
-  )
+
 
 
