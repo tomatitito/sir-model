9777126410e4705a35000de55cc750655924e8e4
hpcjkour <hpcjkour@hlrs.de>
2017-12-07 11:03:36 +0100

    Plot opaque points instead of lines

--- RCode/plot_results.R
+++ RCode/plot_results.R
@@ -2,14 +2,16 @@ library(ggplot2)
 
 
 plot_results = function(path){
-  dat <- read.csv(path,colClasses=c("factor", "numeric", "factor"),header=FALSE)
+  dat <- read.csv(path,colClasses=c("numeric", "numeric", "factor"),header=FALSE)
 
   ## dat needs three columns:
   ## week, cases, simulation-index
   names(dat) <- c("week", "cases", "sim_id")
 
-  p <- ggplot(dat, aes(week, cases)) + geom_line(aes(group=sim_id),alpha=1/10) 
+  p <- ggplot(dat, aes(week, cases)) +
+  #geom_line(aes(group=sim_id),alpha=1/10) +
+    geom_point(alpha=1/10) + geom_smooth(aes(group=1))
   ggsave(file="season.pdf")
 }
 
-plot_results("~/sir-model/data/seasons.csv")
+plot_results("~/sir-model/data/testdat.csv")
