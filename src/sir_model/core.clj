96fe35b52d8dcf1d71e24a93dce1aacade439b21
hpcjkour <hpcjkour@hlrs.de>
2017-12-14 14:42:37 +0100

    Function to sample from the model
    The function lazy-samples now samples from the model and returns a
    lazy-seq. So far, only the query is passed as an argument.

--- src/sir_model/core.clj
+++ src/sir_model/core.clj
@@ -13,7 +13,7 @@
 (def arg-map
   {:t-max        40
    :compartments [:S :I :R :primary :secondary]
-   :inits        {:S 100000 :I 40}
+   :inits        {:S 1000000 :I 400}
    :prior-1      (uniform-continuous 0.4 0.9)
    :prior-2      (uniform-continuous 1.5 2.5)
    })
@@ -24,11 +24,21 @@
     (take n)
     (take-nth thin)))
 
-(def samples (doquery :smc model/two-stage-poisson-query [arg-map model/form-and-prog] :number-of-particles 1000))
-;(def burned (take-nth 4 (take 1000 samples)))
+(defn lazy-samples
+  "Takes an anglican query and returns samples as a lazy-seq."
+  [anglican-query]
+  (doquery :smc anglican-query [arg-map model/form-and-prog] :number-of-particles 1000))
+
+(defn from-query
+  "Takes the result from an anglican doquery and an optional thinning parameter
+  and extracts n elements out of that lazy-seq."
+  ([query-results n]
+   (from-query query-results n 1))
+  ([query-result n thin]
+   (pmap #(nth query-result %) (range 0 n thin))))
 
 (defn from-query-agent
-  "Same as from query but using agents."
+  "Same as from-query but using agents."
   ([query-result n]
    (from-query-agent query-result n 1))
   ([query-result n thin]
@@ -39,21 +49,22 @@
        (send agt conj (nth (rest query-result) i)))
      @agt)))
 
-(defn from-query
-  "Takes the result from an anglican doquery and an optional thinning parameter
-  and extracts n elements out of that lazy-seq."
-  ([query-results n]
-    (from-query query-results n 1))
-  ([query-result n thin]
-   (pmap #(nth query-result %) (range 0 n thin))))
+(def samples
+  (->
+    (lazy-samples model/two-stage-poisson-query)
+    (from-query 5)))
+;(def burned (time (from-query-agent samples 10 1)))
 
-(def burned (from-query samples 5000 4))
 
 (defn -main
   "Probabilistic SIR-Model"
   [& args]
 
-  (util.functions/write-seasons! burned util.functions/new-infections "data/testdat.csv"))
+  ;(util.functions/write-seasons! burned util.functions/new-infections "data/season.csv")
+  )
+
+
+
 
 
 
