024c70a72c68727bd71da693c4606590f4d21588
hpcjkour <hpcjkour@hlrs.de>
2017-12-14 11:13:04 +0100

    Parallelize sampling from the prior
    Two version two get samples from the prior in parallel, using pmap and
    an agent.

--- src/sir_model/core.clj
+++ src/sir_model/core.clj
@@ -13,23 +13,53 @@
 (def arg-map
   {:t-max        40
    :compartments [:S :I :R :primary :secondary]
-   :inits        {:S 25000 :I 25}
+   :inits        {:S 100000 :I 40}
    :prior-1      (uniform-continuous 0.4 0.9)
    :prior-2      (uniform-continuous 1.5 2.5)
    })
-(def run-model #(doquery :smc model/two-stage-poisson-query [arg-map model/form-and-prog] :numerb-of-particles 1000))
-(def s-1 (.start (Thread. run-model)))
-(def s-2 (.start (Thread. run-model)))
-;(def samples (doquery :smc model/two-stage-poisson-query [arg-map model/form-and-prog] :number-of-particles 1000))
+
+(defn prior [n thin]
+  (->>
+    (doquery :smc model/two-stage-poisson-query [arg-map model/form-and-prog] :number-of-particles 1000)
+    (take n)
+    (take-nth thin)))
+
+(def samples (doquery :smc model/two-stage-poisson-query [arg-map model/form-and-prog] :number-of-particles 1000))
 ;(def burned (take-nth 4 (take 1000 samples)))
+
+(defn from-query-agent
+  "Same as from query but using agents."
+  ([query-result n]
+   (from-query-agent query-result n 1))
+  ([query-result n thin]
+   (let
+     [agt (agent (lazy-seq (first query-result)))
+      n-runs (* thin n)]
+     (doseq [i (range 0 n-runs thin)]
+       (send agt conj (nth (rest query-result) i)))
+     @agt)))
+
+(defn from-query
+  "Takes the result from an anglican doquery and an optional thinning parameter
+  and extracts n elements out of that lazy-seq."
+  ([query-results n]
+    (from-query query-results n 1))
+  ([query-result n thin]
+   (pmap #(nth query-result %) (range 0 n thin))))
+
+(def burned (from-query samples 5000 4))
 
 (defn -main
   "Probabilistic SIR-Model"
   [& args]
 
-  (util.functions/write-seasons! burned util.functions/new-infections "data/season.csv") )
+  (util.functions/write-seasons! burned util.functions/new-infections "data/testdat.csv"))
 
-(-main)
+
+
+
+
+
 
 
 
