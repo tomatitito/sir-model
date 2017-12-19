9db57d0df5ba1f8a8088a7f0f278d4abc9dbbdc8
hpcjkour <hpcjkour@hlrs.de>
2017-12-19 15:28:33 +0100

    Remove unused dependencies.

--- project.clj
+++ project.clj
@@ -6,9 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.3"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [proto-repl "0.3.1"]
-                 [proto-repl-charts "0.3.2"]
-                 [proto-repl-sayid "0.1.3"]
                  [anglican "1.0.0"]
                  [lein-gorilla "0.4.0"]]
   :main sir-model.core
