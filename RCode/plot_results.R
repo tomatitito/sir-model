b78706279fd355bfcf27ff7cdc27c8c4db3029c5
hpcjkour <hpcjkour@hlrs.de>
2018-02-05 16:30:29 +0100

    Plot aggregates
    Mean, median and mode are now plotted nicely on top of new infections. A
    new plot with each of these statistics is drawn. Also, weeky
    distributions are now plotted with the scales=free argument and without.
    This way it becomes possibly to compare the central tendencies as well
    as how narrow or wide the distributions are.

--- RCode/plot_results.R
+++ RCode/plot_results.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(dplyr)
 library(purrr)
 library(scales)
+library(tidyr)
 
 args <- commandArgs(TRUE)
 
@@ -52,7 +53,7 @@ mode <- function(v) {
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
-# for mean, mode and median
+# higher order function to compute mean, mode and median
 compute_stat = function(df, f){
   df %>%
     split(.$week) %>%
@@ -60,79 +61,35 @@ compute_stat = function(df, f){
 }
 
 # layer data for aggregate statistics
-stats = data.frame(means=compute_stat(dat, mean), medians=compute_stat(dat, median), modes=compute_stat(dat, mode))
-print(head(stats))
-print(str(stats))
-
-# Multiple plot function
-# from http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
-#
-# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
-# - cols:   Number of columns in layout
-# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
-#
-# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
-# then plot 1 will go in the upper left, 2 will go in the upper right, and
-# 3 will go all the way across the bottom.
-#
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  library(grid)
-
-  # Make a list from the ... arguments and plotlist
-  plots <- c(list(...), plotlist)
-
-  numPlots = length(plots)
-
-  # If layout is NULL, then use 'cols' to determine layout
-  if (is.null(layout)) {
-    # Make the panel
-    # ncol: Number of columns of plots
-    # nrow: Number of rows needed, calculated from # of cols
-    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-    ncol = cols, nrow = ceiling(numPlots/cols))
-  }
-
-  if (numPlots==1) {
-    print(plots[[1]])
-
-  } else {
-    # Set up the page
-    grid.newpage()
-    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-
-    # Make each plot, in the correct location
-    for (i in 1:numPlots) {
-      # Get the i,j matrix positions of the regions that contain this subplot
-      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-
-      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-      layout.pos.col = matchidx$col))
-    }
-  }
-}
+stats_wide = data.frame(week=0:39,means=compute_stat(dat, mean), medians=compute_stat(dat, median), modes=compute_stat(dat, mode))
+stats_long = gather(stats_wide, key=stat, value=val, means:modes)
 
 p_new <- ggplot(data=dat, aes(x=week, y=cases)) +
-  geom_point(alpha=1/100) + #geom_smooth(aes(group=1))
-  # geom_line(aes(group=sim_id), alpha=1/10) +
-  #stat_summary(geom="point", fun.y=mode, color="magenta") +
-  #stat_summary(geom="point", fun.y=mean, color="orange") +
-  #stat_summary(geom="point", fun.y=median, color="green") +
+  geom_point(alpha=1/100) + 
   geom_ribbon(data=borders, aes(week, ymin=lo, ymax=hi), fill="blue", alpha=1/10, inherit.aes=FALSE) +
   scale_y_continuous(labels = comma) +
   labs(title="Simulierte Neuerkrankungen ueber 40 Wochen", x="Woche", y="Neuerkrankungen") +
-  #annotate("text", color="magenta", label="mode", x=39, y=400000) +
-  #annotate("text", color="orange", label="mean", x=39, y=380000) +
-  #annotate("text", color="green", label="median", x=39.3, y=360000) + 
-  geom_line(data=stats, aes(x=1:nrow(stats),y=means, color="mean"), inherit.aes=FALSE) +
-  geom_line(data=stats, aes(x=1:nrow(stats),y=medians, color="median"), inherit.aes=FALSE) +
-  geom_line(data=stats, aes(x=1:nrow(stats),y=modes, color="mode"), inherit.aes=FALSE) 
+  geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=means, color="mean"), inherit.aes=FALSE) +
+  geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=medians, color="median"), inherit.aes=FALSE) +
+  geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=modes, color="mode"), inherit.aes=FALSE) 
+
+p_stats <- ggplot(stats_long, aes(x=week,y=val, color=stat)) + geom_point(show.legend=FALSE) +
+  facet_grid(stat ~ .) +
+  theme(axis.title.x=element_blank(), axis.title.y=element_blank())
 
 weekly_dists <- ggplot(data=dat, aes(cases)) +
   geom_histogram() +
-  facet_wrap(~ week, ncol=7, scales="free") +
+  facet_wrap(~ week, ncol=7) + 
   theme(axis.text.x=element_text(size=5)) +
   labs(title="Verteilung der simulierten Neuerkrankungen pro Woche")
 
-print(max(as.numeric(dat$sim_id)))
+weekly_dists_free <- ggplot(data=dat, aes(cases)) +
+  geom_histogram() +
+  facet_wrap(~ week, ncol=7, scales="free") + 
+  theme(axis.text.x=element_text(size=5)) +
+  labs(title="Verteilung der simulierten Neuerkrankungen pro Woche")
+
 ggsave(file="season_test.pdf", plot=p_new, height=5, width=10)
+ggsave(file="stats_test.pdf", plot=p_stats, height=2.5, width=7)
 ggsave(file="weekly_dists_test.pdf", plot=weekly_dists, height=9, width=11)
+ggsave(file="weekly_dists_free_test.pdf", plot=weekly_dists_free, height=9, width=11)
