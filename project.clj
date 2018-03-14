d28b7f79f26fe24363a702cb303a7b3716c63507
hpcjkour <hpcjkour@hlrs.de>
2018-03-14 15:18:40 +0100

    Add oz as dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [metasoarous/oz "1.3.1"]
                  [org.clojure/data.csv "0.1.3"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [anglican "1.0.0"]
