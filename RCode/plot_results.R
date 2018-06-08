b9989112616e0ba56e18585a74c953f50d083c87
hpcjkour <hpcjkour@hlrs.de>
2018-06-08 14:15:10 +0200

    Use functions instead of harcoded borders and season plot
    Call the functions with a dataframe to produce a plot instead of
    producing the plots in the script and writing them to disk.

--- RCode/plot_results.R
+++ RCode/plot_results.R
@@ -4,7 +4,7 @@ library(purrr)
 library(scales)
 library(tidyr)
 
-args <- commandArgs(TRUE)
+#args <- commandArgs(TRUE)
 
 ## NOTE: when naming cols or changing types 
 ##the way the data looks has changed
@@ -14,7 +14,7 @@ args <- commandArgs(TRUE)
 ## week, new, S, I, R, primary, secondary, sim_id
 
 #path = "data/2018-01-31T22:37:09.770_two-stage-poisson-query_80000000_5000_1000.csv"
-path <- args[1]
+#path <- args[1]
 # dat <- read.csv(path,colClasses=c("numeric", "numeric","factor"),header=TRUE)
 dat <- read.csv(path,colClasses="numeric",header=TRUE) %>% mutate(cases=new )
 
@@ -43,10 +43,17 @@ hdi = function( sampleVec , credMass=0.95 ) {
   return( HDIlim )
 }
 
-borders <- dat %>%
-  split(.$week) %>%
-  map_df(~hdi( .$cases, 0.95)) %>%
-  cbind(week=0:max(dat$week))
+# borders <- dat %>%
+#   split(.$week) %>%
+#   map_df(~hdi( .$cases, 0.95)) %>%
+#   cbind(week=0:max(dat$week))
+
+borders = function(data){
+  data %>%
+    split(.$week) %>%
+    map_df(~hdi( .$cases, 0.95)) %>%
+    cbind(week=0:max(dat$week))
+}
 
 mode <- function(v) {
   uniqv <- unique(v)
@@ -64,33 +71,39 @@ compute_stat = function(df, f){
 stats_wide = data.frame(week=0:39,means=compute_stat(dat, mean), medians=compute_stat(dat, median), modes=compute_stat(dat, mode))
 stats_long = gather(stats_wide, key=stat, value=val, means:modes)
 
-p_new <- ggplot(data=dat, aes(x=week, y=cases)) +
-  geom_point(alpha=1/100) + 
-  geom_ribbon(data=borders, aes(week, ymin=lo, ymax=hi), fill="blue", alpha=1/10, inherit.aes=FALSE) +
-  scale_y_continuous(labels = comma) +
-  labs(title="Simulierte Neuerkrankungen ueber 40 Wochen", x="Woche", y="Neuerkrankungen") +
-  geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=means, color="mean"), inherit.aes=FALSE) +
-  geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=medians, color="median"), inherit.aes=FALSE) +
-  geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=modes, color="mode"), inherit.aes=FALSE) +
-  theme(legend.title=element_blank())
+# p_new <- ggplot(data=dat, aes(x=week, y=cases)) +
+#   geom_point(alpha=1/100) +
+#   geom_ribbon(data=borders, aes(week, ymin=lo, ymax=hi), fill="blue", alpha=1/10, inherit.aes=FALSE) +
+#   scale_y_continuous(labels = comma) +
+#   labs(title="Simulierte Neuerkrankungen ueber 40 Wochen", x="Woche", y="Neuerkrankungen") +
+#   geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=means, color="mean"), inherit.aes=FALSE) +
+#   geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=medians, color="median"), inherit.aes=FALSE) +
+#   geom_line(data=stats_wide, aes(x=1:nrow(stats_wide),y=modes, color="mode"), inherit.aes=FALSE) +
+#   theme(legend.title=element_blank())
 
-p_stats <- ggplot(stats_long, aes(x=week,y=val, color=stat)) + geom_point(show.legend=FALSE) +
-  facet_grid(stat ~ ., scales="free") +
-  theme(axis.title.x=element_blank(), axis.title.y=element_blank())
+season_plot = function(data){
+  ggplot(data=data, aes(x=week, y=cases)) +
+    geom_point(alpha=1/100) +
+    geom_ribbon(data=borders_f(data), aes(week, ymin=lo, ymax=hi), fill="blue", alpha=1/10, inherit.aes=FALSE)
+}
 
-weekly_dists <- ggplot(data=dat, aes(cases)) +
-  geom_histogram() +
-  facet_wrap(~ week, ncol=7) + 
-  theme(axis.text.x=element_text(size=5)) +
-  labs(title="Verteilung der simulierten Neuerkrankungen pro Woche")
-
-weekly_dists_free <- ggplot(data=dat, aes(cases)) +
-  geom_histogram() +
-  facet_wrap(~ week, ncol=7, scales="free") + 
-  theme(axis.text.x=element_text(size=5)) +
-  labs(title="Verteilung der simulierten Neuerkrankungen pro Woche")
-
-ggsave(file="season.pdf", plot=p_new, height=5, width=10)
-ggsave(file="stats.pdf", plot=p_stats, height=2.5, width=7)
-ggsave(file="weekly_dists.pdf", plot=weekly_dists, height=9, width=11)
-ggsave(file="weekly_dists_free.pdf", plot=weekly_dists_free, height=9, width=11)
+# p_stats <- ggplot(stats_long, aes(x=week,y=val, color=stat)) + geom_point(show.legend=FALSE) +
+#   facet_grid(stat ~ ., scales="free") +
+#   theme(axis.title.x=element_blank(), axis.title.y=element_blank())
+#
+# weekly_dists <- ggplot(data=dat, aes(cases)) +
+#   geom_histogram() +
+#   facet_wrap(~ week, ncol=7) +
+#   theme(axis.text.x=element_text(size=5)) +
+#   labs(title="Verteilung der simulierten Neuerkrankungen pro Woche")
+#
+# weekly_dists_free <- ggplot(data=dat, aes(cases)) +
+#   geom_histogram() +
+#   facet_wrap(~ week, ncol=7, scales="free") +
+#   theme(axis.text.x=element_text(size=5)) +
+#   labs(title="Verteilung der simulierten Neuerkrankungen pro Woche")
+#
+# ggsave(file="season_new.pdf", plot=p_new, height=5, width=10)
+# ggsave(file="stats_new.pdf", plot=p_stats, height=2.5, width=7)
+# ggsave(file="weekly_dists_new.pdf", plot=weekly_dists, height=9, width=11)
+# ggsave(file="weekly_dists_free_new.pdf", plot=weekly_dists_free, height=9, width=11)
