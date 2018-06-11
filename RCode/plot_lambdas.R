6fe74dc08c48489ae015dd9394d6a51f0b4cc71e
hpcjkour <hpcjkour@hlrs.de>
2018-06-11 15:35:52 +0200

    Compute priors in script
    Instead of loading them from a file. Also minor changes in naming
    variables in plot. Use underscores instead of points (lambda_1 instead
    of lambda.1).

--- RCode/plot_lambdas.R
+++ RCode/plot_lambdas.R
@@ -8,16 +8,15 @@ library(ggplot2)
 args <- commandArgs(TRUE)
 
 posterior_path <- args[1]
-prior_path <- args[2]
+# prior_path <- args[2]
 
 posterior <- read.csv(posterior_path, header=T)
-prior <- read.csv(prior_path, header=T)
-
+prior <- data.frame(prior_lambda_1=runif(10000, min=0.4,max=0.9), prior_lambda_2=runif(10000, min=1.0,max=1.9))
 all <- cbind(posterior, prior)
-all_long = gather(all, key=c("lambda.1", "lambda.2", "l_1_prior", "l_2_prior"))
-names(all_long) = c("variable", "value")
+all_long = gather(all, key="variable")
 
 p = ggplot(all_long, aes(value)) +
     geom_histogram(aes(y=..density..), bins=20) +
-    facet_wrap(~ variable, ncol=2)
+    facet_wrap(~ variable, ncol=2) +
+    theme(axis.title.x=element_blank(), axis.title.y=element_blank())
 ggsave(file="lambdas.pdf", plot=p)
