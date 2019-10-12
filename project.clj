11bad649f5dbdc2ec95ee74b54656b08c03ec76d
hpcjkour <hpcjkour@hlrs.de>
2019-10-12 23:41:55 +0200

    User clojure 1.9.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [metasoarous/oz "1.3.1"]
                  [org.clojure/data.csv "0.1.3"]
                  [org.clojure/tools.namespace "0.2.11"]
